a63fb17a8cf95a926d49cc93f83847814bbee43c
enriquemore587 <jose.vergara2104@hotmail.com>
2019-09-27 19:45:23 -0500

    first commit

--- APP10/ViewController.swift
+++ APP10/ViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var personas = ["jose", "enrique", "vergara", "ambriz", "jose jose", "enrique", "juan", "mariana"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //MARK: Métodos de DataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return personas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "celda", for: indexPath)
+        if indexPath.row % 2 == 0 {
+            cell.textLabel?.textColor = .white
+            cell.backgroundColor = .black
+        }
+        cell.textLabel?.text = personas[indexPath.row].uppercased()
+        cell.imageView?.image = UIImage(named : "mexico")
+        return cell
+    }
+    
+    
+    //MARK: Métodos del delegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("💩💩💩💩💩💩💩💩💩💩💩💩💩💩")
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
 
 }
-
